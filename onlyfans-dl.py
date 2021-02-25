ee6053307794afc6d4e03e97062055d0699b5481
Tim Hesse <tim.hesse@uni-oldenburg.de>
2021-02-25 15:57:38 +0100

    Updated User-Agent

--- onlyfans-dl.py
+++ onlyfans-dl.py
@@ -40,7 +40,7 @@ PROFILE_ID = ""
 
 API_HEADER = {
     "Accept": "application/json, text/plain, */*",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
     "Accept-Encoding": "gzip, deflate"
 }
 
